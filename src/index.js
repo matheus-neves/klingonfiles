9b3949c030850740d939c6d8b9cfd084a9488943
Matheus Neves <matheus.neves@prodigious.com>
2019-09-03 15:33:44 -0300

    feature to get others fonts

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ const path = require('path');
 const fs = require('fs');
 const request = require('request');
 
-const urls = [];
+const urls = [
+];
 const reqUrls = [];
 
 runPuppeteer = async urls => {
@@ -24,7 +25,29 @@ runPuppeteer = async urls => {
       if (url.host == responseUrl.host &&
         !response.url().includes("browser-sync") &&
         response.url().charAt(response.url().length - 1) != '/') {
-        reqUrls.push(response.url());
+
+        let parse = path.parse(response.url());
+
+        if(parse.ext == '.woff') {
+          
+          let { dir, name} = parse;
+
+          let arrayFonts = [
+            `${dir}/${name}.woff`,
+            `${dir}/${name}.woff2`,
+            `${dir}/${name}.svg`,
+            `${dir}/${name}.ttf`,
+            `${dir}/${name}.eot`,
+          ]
+
+          reqUrls.push(...arrayFonts);
+
+        } else {
+
+          reqUrls.push(response.url());
+        }
+
+        
       }
       response.continue();
     });
@@ -40,16 +63,13 @@ runPuppeteer = async urls => {
 
 }
 
+
+
+
 generateBuild = async url => {
-
-  
-
   url = new URL(url);
-
   
-
-  pathname = url.pathname.replace(/^\.*\/|\/?[^\/]+\.[a-z]+|\/$/g, '');
-  
+  pathname = path.parse(url.pathname).dir.replace(/^\.*\/|\/?[^\/]+\.[a-z]|\/$/g, '');
 
   await fs.mkdirSync(path.resolve(__dirname, '..', 'build', pathname), { recursive: true }, e => {
     if (e) {
@@ -59,34 +79,38 @@ generateBuild = async url => {
     }
   });
 
-
-  let file = await fs.createWriteStream(path.resolve(__dirname, '..', 'build', url.pathname.substring(1)));
-
-
   await new Promise( async (resolve, reject) => {
 
-    await request({
+    let response = await request({
       url: url.href,
-      'auth': { 'user': '', 'pass': ''}})
-    .pipe(file)
-    .on('finish', () => {
-      console.log('Success Downloading: ' + file.path);
-      resolve();
-    })
-    .on('error', (error) => {
+      'auth': { 'user': '', 'pass': ''}});
+
+
+    response.on('response', (res) => {
+      
+      if(res.statusCode == 200) {
+        let file = fs.createWriteStream(path.resolve(__dirname, '..', 'build', url.pathname.substring(1)));
+        console.log(`Success on downloading: ${file.path}`);
+        response.pipe(file);
+        resolve();
+      } 
+
+      if(res.statusCode == 404) {
+        reject(`(404) ${url.href}`);
+      }
+
+    }).on('error', (error) => {
       reject(error);
     })
+
+    
   }).catch(error => {
-    console.log(`Error on Downloading: ${error}`);
+    console.log(`Error on downloading: ${error}`);
   });
 
 }
 
 runPuppeteer(urls).then( () => {
-
   let filteredUrls = Array.from(new Set(reqUrls))
-
-  filteredUrls.map(url => {
-    generateBuild(url);
-  })
+  filteredUrls.map( url =>  generateBuild(url) )
 })
