f668e509fa7a7c1d62a914abcebfc37cbaa32d2a
Matheus Neves <matheus.neves@prodigious.com>
2020-01-03 16:43:06 -0300

    fix routes

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const build = require('./controllers/build.js')
-const routes = new express.Router();
+const routes = express.Router();
 
 routes.post('/build', build);
 
